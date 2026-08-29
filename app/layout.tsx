b8c799b6825916ccac0de0589e6b0b9be0b6f9af
Marcos Lucas <afk.marcos@gmail.com>
2026-08-29 15:08:20 -0300

    fix co

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${inter.className} bg-white text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div
-          className="sm:bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]
-        sm:w-[68.75rem] dark:sm:bg-[#966263]"
-        ></div>
-        <div
-          className="sm:bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]
-        sm:w-[68.75rem] md:left-[-35rem] lg:left-[-28rem] xl-left-[-15rem] 2xl-[-5rem] dark:sm:bg-[#676394]"
-        ></div>
         <div>
           <Navbar />
         </div>
